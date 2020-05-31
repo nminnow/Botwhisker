d7e949f94fb9690a60cc9b8599c2a9cb7e5c0b6a
nminnow <nminnow@outlook.com>
2020-05-31 12:39:05 -0400

    Log script doc also

--- update_log.py
+++ update_log.py
@@ -6,6 +6,7 @@ import sys
 if __name__ == '__main__':
     with open('log.txt', 'a') as f:
         now = datetime.utcnow()
-        for script in sys.argv[1:]:
-            f.write('{0}-{1}-{2} {3}\n'.format(now.year, now.month, now.day,
-                script))
+        for argv in sys.argv[1:]:
+            script = __import__(argv)
+            f.write('{0}-{1}-{2} {3} {4}\n'.format(now.year, now.month, now.day,
+                argv, script.__doc__))
