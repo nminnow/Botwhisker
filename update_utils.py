514981b2d048c6f0c5b7cef12c57237e8c662c5e
nminnow <nminnow@outlook.com>
2020-06-16 14:47:44 -0400

    Update utils, add cp2zh

--- update_utils.py
+++ update_utils.py
@@ -53,7 +53,7 @@ for row in zh2cp_works_query:
         zh2cp_works_results[row.code.value.lower()] = (row.work
             .rpartition('/')[-1])
 
-zh2cp_results = {
+zh2cp = {
     'characters': zh2cp_characters_results,
     'books': zh2cp_books_results,
     'works': zh2cp_works_results
@@ -61,5 +61,24 @@ zh2cp_results = {
 with open('utils/zh2cp.py', 'w') as f:
     f.write('# -*- coding: utf-8 -*-\n\n')
 with open('utils/zh2cp.py', 'a') as f:
-    for key in zh2cp_results:
-        f.write('{0} = {1}\n'.format(key, zh2cp_results[key]))
+    for key in zh2cp:
+        f.write('{0} = {1}\n'.format(key, zh2cp[key]))
+
+cp2zh = {
+    'characters': {},
+    'books': {},
+    'works': {}
+    }
+
+for character in zh2cp_characters_results:
+    cp2zh['characters'][zh2cp_characters_results[character]] = character
+for book in zh2cp_books_results:
+    cp2zh['books'][zh2cp_books_results[book]] = book
+for work in zh2cp_works_results:
+    cp2zh['works'][zh2cp_works_results[work]] = work
+
+with open('utils/cp2zh.py', 'w') as f:
+    f.write('# -*- coding: utf-8 -*-\n\n')
+with open('utils/cp2zh.py', 'a') as f:
+    for key in cp2zh:
+        f.write('{0} = {1}\n'.format(key, cp2zh[key]))
