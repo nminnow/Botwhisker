5fe6279866420e61ca8f15adfa91c7f586f77e69
nminnow <nminnow@outlook.com>
2020-06-19 12:26:24 -0400

    Add main()

--- allegiance_to_characters.py
+++ allegiance_to_characters.py
@@ -4,19 +4,23 @@ import pywikibot
 from pywikibot import pagegenerators
 from utils import zh2cp
 
-site_zh = pywikibot.Site('zh', 'zhwarriorswiki')
-site_cp = pywikibot.Site('en', 'crystalpool')
-repo = site_cp.data_repository()
-book = input('请输入书籍名称（例如“日落和平”）：')
-item = pywikibot.ItemPage(repo, input('请输入书籍实体QID（例如“Q144”）：'))
+def main():
+    site_zh = pywikibot.Site('zh', 'zhwarriorswiki')
+    site_cp = pywikibot.Site('en', 'crystalpool')
+    repo = site_cp.data_repository()
+    book = input('请输入书籍名称（例如“日落和平”）：')
+    item = pywikibot.ItemPage(repo, input('请输入书籍实体QID（例如“Q144”）：'))
 
-characters = []
-for page in pagegenerators.LinkedPageGenerator(pywikibot.Page(site_zh,
-    '{0}/猫物表'.format(book))):
-    if pywikibot.Category(site_zh, 'Category:猫物') in list(page.categories()):
-        characters.append(zh2cp.characters[page.title()])
+    characters = []
+    for page in pagegenerators.LinkedPageGenerator(pywikibot.Page(site_zh,
+        '{0}/猫物表'.format(book))):
+        if pywikibot.Category(site_zh, 'Category:猫物') in list(page.categories()):
+            characters.append(zh2cp.characters[page.title()])
 
-for character in characters:
-    claim = pywikibot.Claim(repo, 'P119') # characters
-    claim.setTarget(pywikibot.ItemPage(repo, character))
-    item.addClaim(claim)
+    for character in characters:
+        claim = pywikibot.Claim(repo, 'P119') # characters
+        claim.setTarget(pywikibot.ItemPage(repo, character))
+        item.addClaim(claim)
+
+if __name__ == '__main__':
+    main()
